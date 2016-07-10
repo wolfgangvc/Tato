fca6bb50105160da1a3bb36a7fd286e0a29e5eb9
Wolfgang Sebastian von Caron <wolfgang@voncaron.co.uk>
2016-07-10 21:39:56 +0100

    Added testing for invalid data in newuser function

--- tests/UserServiceTest.php
+++ tests/UserServiceTest.php
@@ -16,12 +16,29 @@ class UserServiceTest extends \PHPUnit_Framework_TestCase
         $this->userService = new UserService();
     }
 
-    public function testUserServiceNewUser()
+    public function testUserServiceNewUserValid()
     {
         $this->assertEquals(true, $this->userService->newUser("Bob@Mail.com", "bob", "bobpass", "Display Bob"));
         $user = $this->userService->getByID(1);
         if ($user instanceof User) {
             $this->assertEquals("bob", $user->name);
+            $this->assertEquals("bob@mail.com", $user->email);
+            $this->assertEquals("Display Bob", $user->display_name);
+            $this->assertEquals(true, password_verify("bobpass", $user->pass));
         }
+    }
+    public function testUserServiceNewUserInvalid()
+    {
+        // test when no strings are passed
+        $this->assertEquals(false, $this->userService->newUser("", "", ""));
+
+        //test invalid email
+        $this->assertEquals(false, $this->userService->newUser("barbara", "barbara", "barpass"));
+
+        //test invalid username
+        $this->assertEquals(false, $this->userService->newUser("barbara@mail.com", "b", "barpass"));
+
+        //test invalid password
+        $this->assertEquals(false, $this->userService->newUser("barbara@mail.com", "barbara", "bar"));
     }
 }
